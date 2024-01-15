4c6038f96c31b5602368924c9a8f1f1404d8063f
cgbea <151942177+cgbea@users.noreply.github.com>
2024-01-15 17:59:42 +0000

    added highscore table

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,19 +1,21 @@
-// const ol = document.querySelector('ol');
-// var noOfScores = 5;
-// var highScore = "highScores";
+const highScoresList = document.getElementById("highscores");
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const clearButton = document.getElementById("clear");
 
-// const highScoreString = localStorage.getItem(highScore);
-// const highScoresParse = JSON.parse(highScoreString) || [];
+highScoresList.innerHTML = highScores
+  .map(function (score) {
+    return (
+      '<li class="high-score">' +
+      score.initials +
+      " scored " +
+      score.score +
+      "</li>"
+    );
+  })
+  .join("");
 
-// function getInitials() {
-//   return localStorage.getItem("userInitials");
-// }
-// function getScore() {
-//   return localStorage.getItem("score");
-// }
-// function updateHTML() {
-//   var initials = getInitials();
-//   var score = getScore();
-//   document.getElementById("highscores").innerHTML = initials + " scored " + score;
-// }
-// updateHTML();
+// Clearing the highscore table/localStorage
+clearButton.addEventListener("click", function () {
+  localStorage.clear();
+  highScoresList.innerHTML = "";
+});
